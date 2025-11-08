7a7063d787d0535f9472130313febbd8f439c10b
Sourav <ksourav662@gmail.com>
2025-11-08 16:54:03 +0530

    feat(add-post): implement live image preview before upload

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DEFAULT_POST_IMAGE } from 'src/app/constants/image-paths';
 
 @Component({
   selector: 'app-new-post',
@@ -8,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class NewPostComponent implements OnInit {
   constructor() {}
   permalink: string = '';
+  imgSrc: string = DEFAULT_POST_IMAGE;
 
   ngOnInit(): void {}
 
@@ -15,5 +17,16 @@ export class NewPostComponent implements OnInit {
     const inputElement = event.target as HTMLInputElement;
     const title = inputElement.value;
     this.permalink = title.trim().toLowerCase().replace(/\s+/g, '-');
+  }
+
+  showPreview(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (e: ProgressEvent<FileReader>) => {
+        this.imgSrc = e.target?.result as string;
+      };
+      reader.readAsDataURL(input.files[0]);
+    }
   }
 }
